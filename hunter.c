db1a5fba903d2d8b1aa518453ce4e86c74b877bf
Wemyss <wemyss@users.noreply.github.com>
2014-10-28 15:33:41 +1100

    Update hunter.c

--- hunter.c
+++ hunter.c
@@ -9,19 +9,19 @@
 void decideHunterMove(HunterView gameState) {
     PlayerID player = whoAmI(gameState);
     LocationID nextMove = whereIs(gameState, player);
-	LocationID trail[TRAIL_SIZE];
+    LocationID trail[TRAIL_SIZE];
     int numLoc = 0;
-	int *numLocations = &numLoc;
-	LocationID *moveList = whereCanIgo(gameState, numLocations, TRUE, TRUE, TRUE);
+    int *numLocations = &numLoc;
+    LocationID *moveList = whereCanIgo(gameState, numLocations, TRUE, TRUE, TRUE);
     if (numLoc != 0 && howHealthyIs(gameState, player) > 3) {
-	   giveMeTheTrail(gameState, player, trail);
-	   // Compare trail and possible moves, removing those that appear int the trail
-       int j;
-       for (j = 0; j < *numLocations; j++) {
-          if (moveList[j] != NOWHERE) {
-             nextMove = moveList[j];
-          }
-       }
+        giveMeTheTrail(gameState, player, trail);
+	// Compare trail and possible moves, removing those that appear int the trail
+        int j;
+        for (j = 0; j < *numLocations; j++) {
+            if (moveList[j] != NOWHERE) {
+                nextMove = moveList[j];
+            }
+        }
     }
     registerBestPlay(IDToAbbrev(nextMove),"The trill of the hunt!!!");
 }
